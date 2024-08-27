194313a2d7a278d195498eba1175025bb2fc923e
Robin Ghosh <rghosh@atu.edu>
2024-08-26 21:51:41 -0500

    2nd committ

--- hello.cpp
+++ hello.cpp
@@ -6,5 +6,5 @@ int main()
 	cout << "Hello World" << endl;
 	cout << "testing to commit" << endl;
 	//Git Fetch is the command that tells the local repository that there are changes available in the
-remote repository without bringing the changes into the local repository
+//remote repository without bringing the changes into the local repository
 }
